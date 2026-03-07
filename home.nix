27d8bfa2574ebb26b4a83df988c31588a7f045d6
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2026-03-07 19:48:43 +0100

    adding gaming setup

--- home.nix
+++ home.nix
@@ -1,48 +1,18 @@
-{ pkgs, ... }:
+{pkgs, ... }:
 
 {
   imports = [
     ./base.nix
   ];
-  
-  # For sunshine streaming
-  home.file.".xprofile" = {
-    text =''
-      xset -dpms
-      xset s off
-      xset s noblank
-    '';
+
+  nixpkgs.config = {
+    allowUnfree = true; # Needed for vivaldi that is free but not fully opensource.
   };
-
-# Disable KDE monitor sleep (Wayland-safe, fixes Sunshine 503)
-# Disable ALL display sleep in KDE (Wayland-safe)
-# The Migration section I had this on my initial file, no idea if i should keep it.
-  home.file.".config/powermanagementprofilesrc".text = ''
-    [Migration]
-    MigratedProfilesToPlasma6=powerdevilrc
-
-    [AC][DPMSControl]
-    idleTime=0
-    lockBeforeTurnOff=0
-    turnOffDisplayIdleTimeout=0
-
-    [Battery][DPMSControl]
-    idleTime=0
-    lockBeforeTurnOff=0
-    turnOffDisplayIdleTimeout=0
-
-    [LowBattery][DPMSControl]
-    idleTime=0
-    lockBeforeTurnOff=0
-    turnOffDisplayIdleTimeout=0
-    '';
-
-  # Disable screen locking entirely
-  home.file.".config/kscreenlockerrc".text = ''
-    [Daemon]
-    Autolock=false
-    LockOnResume=false
-    Timeout=0
-    '';
+  home.packages = with pkgs; [
+    vivaldi # Browser.
+    kitty # Best terminal.
+    steam # Gaming.
+    heroic # For games on epic gamges GOG etc.
+  ];
 
 }
