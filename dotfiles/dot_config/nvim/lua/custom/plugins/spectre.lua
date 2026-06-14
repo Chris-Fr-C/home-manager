95a07a53d98930e3e2a2623f68caebc16227e902
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2026-06-15 00:38:46 +0200

    spctre

---
+++ dotfiles/dot_config/nvim/lua/custom/plugins/spectre.lua
@@ -0,0 +1,10 @@
+-- Search and replace, sometimes useful.
+-- 
+local containers = require("custom.config.keymap-containers")
+vim.pack.add({
+  "https://github.com/nvim-pack/nvim-spectre",
+})
+local spectre = require("spectre")
+spectre.setup({})
+vim.keymap.set("n", containers.open.key .. "s", spectre.toggle, {desc="[s]earch and replace"})
+return {}
