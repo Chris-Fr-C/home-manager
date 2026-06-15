aeb648534c280bec89a51b2c991f83a6954e5996
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2026-06-15 13:46:19 +0200

    cleanup + yazi

--- base.nix
+++ base.nix
@@ -4,7 +4,7 @@ let
     cfg-nix="nvim ~/.config/home-manager/home.nix";
     find = "fd";
     cd = "z";
-    c = "yazi";
+    c = "y"; # instead of yazi so we can leave and set the dir.
     ls = "eza --icons=always";
     lsl = "ls -l";
     lsls = "lsl --total-size";
@@ -23,62 +23,9 @@ let
     # This one is for interactive shells.
     EDITOR = "nvim";
   };
-  sharedShellInit=''
-      # For management of suspended processes.
-      fgf() {
-        local job
-        job=$(jobs -l | fzf --ansi | awk '{print $1}' | tr -d '[]')
-        if [ -n "$job" ]; then
-          fg %"$job"
-        fi
-      }
-      # Or with gum 
-      fgg() {
-        # List jobs with job number + command
-        local job
-        job=$(jobs -l | awk '{print "[" $1 "] " substr($0, index($0,$3)) }' \
-              | gum choose --height 10)
-
-        if [ -n "$job" ]; then
-          # Extract job number between brackets
-          local job_num=$(echo "$job" | grep -oP '\[\K[0-9]+')
-          fg %"$job_num"
-        fi
-      }
-
-      cfg(){
-        cd ~/.config/home-manager
-      }
-      cfge(){
-        nvim ~/.config/home-manager/dotfiles/dot_config/nvim/init.lua
-      }
-
-
-      refresh() {
-          local dir=~/.config/home-manager
-          # Use fd to list .nix files in the folder, non-recursive
-          local file
-          file=$(fd --type f --extension nix . "$dir" | xargs -n1 basename | gum choose --height 10)
-
-          if [ -n "$file" ]; then
-              echo "Switching to $file..."
-              home-manager switch -f "$dir/$file"
-          else
-              echo "No file selected."
-          fi
-      }
-    term(){
-      ttyd -p 9001 -t fontFamily="JetBrainsMono Nerd Font" --writable zsh & disown
-    }
-
-      eval "$(pay-respects bash --alias)"
-      eval "$(pay-respects zsh --alias)"
-      eval "$(zoxide init bash)"
-      # Not happy of this. I should refactor it. 
-      export PYTHONPATH="$HOME/.local/lib/python3.13/site-packages"
-      export JAVA_HOME="$HOME/.nix-profile/bin"
-
-  '';
+  sharedShellInit=builtins.readFile ./scripts/functions.sh + 
+    builtins.readFile ./scripts/initializations.sh + 
+    builtins.readFile ./scripts/yazi-shortcut.sh;
 in
 {
   # Not using nix
---
+++ scripts/initializations.sh
@@ -0,0 +1,7 @@
+eval "$(pay-respects bash --alias)"
+eval "$(pay-respects zsh --alias)"
+eval "$(zoxide init bash)"
+# Not happy of this. I should refactor it.
+export PYTHONPATH="$HOME/.local/lib/python3.13/site-packages"
+export JAVA_HOME="$HOME/.nix-profile/bin"
+
